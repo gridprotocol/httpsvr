151536d123f07edb18b9c04c4ac5e7adc8c873fb
rocky <rocky@memoio.io>
2025-03-04 18:16:23 +0800

    add alterPayee

--- main.go
+++ main.go
@@ -10,8 +10,10 @@ import (
 var CmdName string
 
 func main() {
-	//
+	// info
 	http.HandleFunc("/", handlerInfo)
+	// alter payee
+	http.HandleFunc("/alterpayee", handlerAlterPayee)
 
 	fmt.Println("Starting server on :8081")
 	if err := http.ListenAndServe(":8081", nil); err != nil {
@@ -19,7 +21,44 @@ func main() {
 	}
 }
 
-// Handler 处理 HTTP 请求
+// alter payee to a grid user address
+func handlerAlterPayee(w http.ResponseWriter, r *http.Request) {
+
+	// 获取查询参数中的 user 值
+	user := r.URL.Query().Get("user")
+	if user == "" {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusBadRequest)
+		w.Write([]byte("Missing 'user' query parameter"))
+		return
+	}
+
+	fmt.Println("grid user address:", user)
+
+	// check user or provider
+	cmdName, err := UserOrProvider()
+	if err != nil {
+		w.Header().Set("Content-Type", "text/plain")
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+	}
+
+	// alter payee cmd arg
+	arg := "settle alterPayee --really-do-it=true " + user
+
+	cmd := exec.Command(cmdName, arg)
+	output, err := cmd.CombinedOutput()
+	if err != nil {
+		http.Error(w, fmt.Sprintf("Error: %v", err), http.StatusInternalServerError)
+		return
+	}
+
+	w.Header().Set("Content-Type", "text/plain")
+	w.WriteHeader(http.StatusOK)
+	w.Write([]byte(output))
+}
+
+// info
 func handlerInfo(w http.ResponseWriter, r *http.Request) {
 	// check user or provider
 	cmdName, err := UserOrProvider()
